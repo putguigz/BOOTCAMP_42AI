d31ffa3c3e16deab1f60f5d0dc27905fd52aaf00
gpetit <gpetit@student.42.fr>
2021-11-09 01:03:49 +0100

    morse debut

---
+++ PY00/ex08/sos.py
@@ -0,0 +1,15 @@
+import sys
+
+morse = {
+	    'A' : '.-', 'B' : '-...', 'C' : '-.-.', 'D' : '-..', 'E' : '.', 'F' : '..-.', 'G' : '--.', 'H' : '....', 'I' : '..', 'J' : '.---', 'K' : '-.-', 'L' : '.-..', 'M' : '--', 'N' : '-.', 'O' : '---', 'P' : '.--.', 'Q' : '--.-', 'R' : '.-.', 'S' : '...', 'T' : '-', 'U' : '..-', 'V' : '...-', 'W' : '.--', 'X' : '-..-', 'Y' : '-.--', 'Z' : '--..'
+		}
+
+for word in sys.argv[1:]:
+	if not word.isalnum() or not word.isupper():
+		print("ERROR")
+		quit()
+
+for word in sys.argv[1:]:
+		for letter in word:
+			print(morse[letter], end=' ')
+
