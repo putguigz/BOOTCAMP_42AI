a9aac996a1dc399c37ff2174cf688c7ccd367c43
Guillaume PETIT <gpetit@e1r6p18.42.fr>
2021-11-11 19:28:46 +0100

    corrected compute of time elapsed

--- PY00/ex10/loading.py
+++ PY00/ex10/loading.py
@@ -1,32 +1,41 @@
-from time import sleep
+from time import *
 
 def ft_progress(listy):
-	'''RETURN POSITION IN RANGE'''	
+	'''RETURN POSITION IN RANGE'''
+	
+	time_elapsed = 0
+	sleep_value = 0.01
+	range_size = len(listy)
+	remaining_range = range_size
 	for i in listy:
+		start = time()
+		time_elapsed += sleep_value
+		remaining_range-=1
+		percent = (range_size - remaining_range) * 100 / range_size
+		ETA = float(sleep_value * remaining_range)
+		size_loading_bar = 50
+		size_stars = int(percent * size_loading_bar / 100)
+		size_spaces = int(size_loading_bar - size_stars)
+		sequence = "{:" + '*' + "<" + str(size_stars) + "}"
+		sequence2 = "{: <" + str(size_spaces) + "}"
+		progress_bar = sequence.format("") + sequence2.format("")
+		print("ETA: %.2fs [ %.2d%%]" % (ETA, percent), "%s/%i |" % ("{: >4}".format(range_size - remaining_range), range_size), "[" + progress_bar + "]", "elapsed_time %.2fs" % time_elapsed, end='\r')
 		yield i
+		end = time() - start
 
-time_elapsed = 0
-sleep_value = 0.01
-range_size = 1000
-remaining_range = range_size
 
-listy = range(range_size)
+listy = range(1000)
 ret = 0
 for elem in ft_progress(listy):
-	time_elapsed += sleep_value
-	remaining_range-=1
-	percent = (range_size - remaining_range) * 100 / range_size
-	ETA = float(sleep_value * remaining_range)
-	size_loading_bar = 50
-	size_stars = int(percent * size_loading_bar / 100)
-	size_spaces = size_loading_bar - size_stars
-	sequence = "{:" + '*' + "<" + str(size_stars) + "}"
-	sequence2 = "{: <" + str(size_spaces) + "}"
-	progress_bar = sequence.format("") + sequence2.format("")
-	print("ETA: %.2fs [ %s%%]" % (ETA, "{: >4}".format(percent)), "%s/%i |" % ("{: >4}".format(range_size - remaining_range), range_size), "[" + progress_bar + "]", "elapsed_time %.2fs" % time_elapsed, end='\r')
-
-	
 	ret += (elem + 3) % 5
-	sleep(sleep_value)
+	sleep(0.01)
+print()
+print(ret)
+
+listy = range(3333)
+ret = 0
+for elem in ft_progress(listy):
+	ret += elem
+	sleep(0.005)
 print()
 print(ret)
