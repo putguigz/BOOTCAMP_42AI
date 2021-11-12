3a29f14dae2530838713467788bbbf3dccf84232
Guillaume PETIT <gpetit@e1r5p17.42.fr>
2021-11-12 16:56:39 +0100

    now using matplotlib

--- PY03/ex01/ImageProcessor.py
+++ PY03/ex01/ImageProcessor.py
@@ -1,4 +1,5 @@
-from PIL import Image
+from matplotlib import image
+from matplotlib import pyplot
 import numpy as np
 import os
 
@@ -7,25 +8,26 @@ class ImageProcessor:
 	@staticmethod
 	def load(path):
 		try:
-			image = Image.open(path)
 			if os.stat(path).st_size == 0:
 				raise OSError("Empty File")
-			print(f"Loading image of dimensions {image.size[0]} x {image.size[1]}")
-			return (np.asarray(image))
+			image_arr = image.imread(path)
+			print(f"Loading image of dimensions {image_arr.shape[0]} x {image_arr.shape[1]}")
+			return (image_arr)
 		except FileNotFoundError or OSError as err:
 			print(err.args)
 
+	@staticmethod
 	def display(array):
 		try:
 			if not isinstance(array, np.ndarray):
 				raise ValueError("Input is of WrongType")
-			img = Image.fromarray(array)
-			img.show()
+			pyplot.imshow(array)
+			pyplot.show()
 		except ValueError as err:
 			print(err.args)
 
 
-img = ImageProcessor.load("42AI.png")
+img = ImageProcessor.load("elon_canaGAN.png")
 print(type(img))
 print(img)
 ImageProcessor.display(img)
