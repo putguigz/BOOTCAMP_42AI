9fa9e0c08bd1518340fab310ef37aafa768f93d0
Guillaume PETIT <gpetit@e1r6p13.42.fr>
2021-11-18 18:01:55 +0100

    Yepeeee

--- PY03/ex04/Kmeans.py
+++ PY03/ex04/Kmeans.py
@@ -8,6 +8,9 @@ from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import math
 
+def get_cmap(n, name='hsv'):
+    return plt.cm.get_cmap(name, n)
+
 class KmeansClustering:
 	def __init__(self, max_iter=20, ncentroid=4):
 		if not isinstance(ncentroid, int) or not isinstance(max_iter, int):
@@ -16,13 +19,17 @@ class KmeansClustering:
 			raise ValueError("Negative or equal to zero parameters")
 		self.ncentroid = ncentroid
 		self.max_iter = max_iter
-		self.centroids = []				
+		self.centroids = []
+		self.distances = []
+		self.prediction = []
 
 	def shortest_distance_index(self, centroids, elem):
 		dico = {}
 		for i in range(self.ncentroid):
 			dico[i] = math.sqrt((centroids[i][0] - elem[0]) ** 2 + (centroids[i][1] - elem[1]) ** 2 + (centroids[i][2] - elem[2]) ** 2)
-		return (min(dico, key=dico.get))	
+		idx = min(dico, key=dico.get)
+		distance = dico[idx]
+		return idx, distance	
 
 	def fit(self, X):
 		if not isinstance(X, np.ndarray):
@@ -32,25 +39,30 @@ class KmeansClustering:
 			a_centroids = np.ndarray((self.max_iter, self.ncentroid, 3))
 			for i in range(self.max_iter):
 				for j in range(self.ncentroid):
-					a_centroids[i][j] = X[random.randint(0, len(X))]
+					a_centroids[i][j] = X[random.randint(0, len(X) - 1)]
 			self.a_centroids = a_centroids
 			for i in range(self.max_iter):
 				self.current_iter = i
-				prediction = self.predict(X)
-			
-			colors = ["red", "green", "blue", "black"]
+				self.prediction.append(self.predict(X))
+			k = 0
+			while k < self.max_iter and self.distances[k] != min(self.distances):
+				k += 1
+			colors = get_cmap(self.ncentroid + 1)
 			ax = plt.axes(projection='3d')
 			for i, elem in enumerate(X):
-				ax.scatter3D(elem[0], elem[1], elem[2], color=colors[prediction[i][0]])
+				ax.scatter3D(elem[0], elem[1], elem[2], color=colors(self.prediction[k][i][0]))
 			plt.show()
 			
 			
 
 	def predict(self, X):
+		dis = []
 		predict = np.ndarray((np.shape(X)[0], 1))
 		for j, elem in enumerate(X):
 			idx = self.shortest_distance_index(self.a_centroids[self.current_iter], elem)
-			predict[j] = idx
+			predict[j] = idx[0]
+			dis.append(idx[1])
+		self.distances.append(sum(dis))
 		predict = predict.astype(int)
 		return predict
 
