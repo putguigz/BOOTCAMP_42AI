18438017ea04c0a0d9adbe645a3ec3ddc682de9c
Guillaume PETIT <gpetit@e1r6p13.42.fr>
2021-11-19 15:03:30 +0100

    patched way of creating subFrames

--- PY04/ex01/YoungestFellah.py
+++ PY04/ex01/YoungestFellah.py
@@ -7,18 +7,16 @@ def youngfellah(df, year):
 	if not isinstance(df, pd.DataFrame):
 		return dico
 	else:
-		year = df[df["Year"] == year]
-		if not year.empty:
-			males = year[year['Sex'] == 'M']
-			females = year[year['Sex'] == 'F']
-			if not males.empty:
-				dico["man"] = df.loc[males["Age"].idxmin()]["Age"]
-			if not females.empty:
-				dico["woman"] = df.loc[females["Age"].idxmin()]["Age"]
+		males = df[(df["Year"] == year) & (df['Sex'] == 'M')]
+		females = df[(df["Year"] == year) & (df['Sex'] == 'F')]
+		if not males.empty:
+			dico["man"] = df.loc[males["Age"].idxmin()]["Age"]
+		if not females.empty:
+			dico["woman"] = df.loc[females["Age"].idxmin()]["Age"]
 		return dico
 
 	
 fl = FileLoader()
 data = fl.load("athlete_events.csv")
-dico  = youngfellah(data, 1991)
+dico  = youngfellah(data, 1992)
 print(dico)
